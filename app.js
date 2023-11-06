460cb8bc09c71e92f11d1a4b144fedd5a0cdfc42
Lyle53 <lyle9453@gmail.com>
2023-11-06 15:00:49 +0800

    build:add static file & json for model

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//M(model)負責與資料集溝通(取得了restaurant.json裡面的資料)
+app.use(express.static('public'))
 
+
+//C 架構的中間人（先處理）
 app.get('/', (req, res) => {
   res.redirect('/restaurant')
 })
